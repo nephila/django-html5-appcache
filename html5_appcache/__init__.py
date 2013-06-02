436101a627cda09043fa8e6b0d7da4d2ffc3fb54
Iacopo Spalletti <i.spalletti@nephila.it>
2013-06-02 19:40:59 +0200

    Bump version

--- html5_appcache/__init__.py
+++ html5_appcache/__init__.py
@@ -3,7 +3,7 @@ from distutils.version import LooseVersion
 from django.utils import importlib
 from .appcache_base import AppCacheManager
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 
 appcache_registry = AppCacheManager()
 
