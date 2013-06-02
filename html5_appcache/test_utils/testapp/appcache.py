aae0d0042460e5e4c5ede6e09b52f8393688aae6
Iacopo Spalletti <i.spalletti@nephila.it>
2013-06-02 19:52:45 +0200

    add multiprotocol urls. Tests fail

--- html5_appcache/test_utils/testapp/appcache.py
+++ html5_appcache/test_utils/testapp/appcache.py
@@ -23,8 +23,9 @@ class NewsAppCache(BaseAppCache):
             urls.append(reverse('news_detail', kwargs={'pk': item.pk}))
             # Adding this to later remove it in get_network
             urls.append(reverse('news_detail_live', kwargs={'pk': item.pk}))
-        urls.append("http://www.example.com/fake-urlake"
-                    "/")
+        urls.append("http://www.example.com/fake-urlake")
+        urls.append("https://www.example.com/fake-urlake")
+        urls.append("ftp://www.example.com/fake-urlake")
         return urls
 
     def signal_connector(self, instance, **kwargs):
